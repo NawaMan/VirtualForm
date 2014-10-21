f9138107ac6bbd74cb77313979e4a666bc6ccb8c
NawaMan <nawa@dssbsoft.com>
2014-10-21 03:28:42 -0600

    VForm can be submit.

--- VirtualForm/script/virtualForm.js
+++ VirtualForm/script/virtualForm.js
@@ -12,11 +12,56 @@ var virtualForm = (function($) {
 		
 	};
 	
+	function extractParameters($target) {
+		var fields = {};
+		var values = {};
+		$target.find("input,select,textarea").each(function() {
+			var $This = $(this);
+			if ($This.is("input[type=button]")) {
+				return;
+			}
+			var name = $This.attr("data-name");
+			if (!name) {
+				name = $This.attr("id");
+			}
+			if (!name) {
+				name = $This.attr("name");
+			}
+			var value;
+			if ($This.is("input[type=checkbox]")) {
+				value = ($This.attr('checked') == 'checked');
+			} else {
+				value = $This.val();
+			}
+			values[name] = value;
+			fields[name] = {
+				'value': value,
+				'element': $This
+			};
+		});
+		return { 'fields': fields, 'values': values };
+	}
+	
 	makeForm = function ($target) {
 		if (!$target) {
 			return;
 		}
 		$target = $($target);
+		
+		$target.bind('callSubmit', function () {
+			var $This = $(this);
+			var result = extractParameters($This);
+			var event = $.Event("submit");
+			event.values = result.values;
+			event.fields = result.fields;
+			$target.trigger(event);
+		});
+		
+		return {
+			'submit': function() {
+				$target.trigger('callSubmit');
+			}
+		};
 	};
 	
 	return {
